eeb1d4253feffa6c09bd62fd5ad10b8515949f0b
Trenton Wesche <TrWesche@live.com>
2020-01-16 14:46:12 -0600

    CodeAcademy A Sorted Tale Complete

--- a_sorted_tale/ca_asortedtale_project.py
+++ a_sorted_tale/ca_asortedtale_project.py
@@ -1,4 +1,66 @@
 import utils
 import sorts
 
+#################################################
+###        Load in short bookshelf order      ###
+#################################################
+
 bookshelf = utils.load_books('books_small.csv')
+bookshelf_v1 = bookshelf.copy()
+bookshelf_v2 = bookshelf.copy()
+print("\n##### Bookshelf Load Complete #####\n")
+
+
+#################################################
+###  Order by title ascending - Bubble Sort   ###
+#################################################
+def by_title_ascending(book_a, book_b):
+    return book_a.get('title_lower') > book_b.get('title_lower')
+
+sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
+for val in sort_1:
+    print(val)
+print("\n ##### Bookshelf Sort by Title Complete #####\n")
+
+
+#################################################
+### Order by author ascending  - Bubble Sort  ###
+#################################################
+def by_author_ascending(book_a, book_b):
+    return book_a.get('author_lower') > book_b.get('author_lower')
+
+sort_2 = sorts.bubble_sort(bookshelf_v1, by_author_ascending)
+for val in sort_2:
+    print(val)
+print("\n ##### Bookshelf Sort by Author Complete #####\n")
+
+
+#################################################
+###   Order by author ascending - Quick Sort  ###
+#################################################
+sorts.quicksort(bookshelf_v2, 0, len(bookshelf_v2) - 1, by_author_ascending)
+for val in bookshelf_v2:
+    print(val)
+print("\n ##### Bookshelf Sort by Author Complete #####\n")
+
+
+
+#################################################
+###        Load in long bookshelf order       ###
+#################################################
+long_bookshelf = utils.load_books('books_large.csv')
+
+def by_total_length(book_a, book_b):
+    return (len(book_a.get('title'))+len(book_a.get('author'))) > (len(book_b.get('title'))+len(book_b.get('author')))
+
+################################################
+##       Order by length - Bubble Sort       ###
+################################################
+
+sort_3 = sorts.bubble_sort(long_bookshelf, by_total_length)
+
+#################################################
+###        Order by length - Quick Sort       ###
+#################################################
+
+sorts.quicksort(long_bookshelf, 0, len(long_bookshelf)-1, by_total_length)
--- a_sorted_tale/utils.py
+++ a_sorted_tale/utils.py
@@ -1,13 +1,23 @@
 import csv
+import os
 
 # This code loads the current book
 # shelf data from the csv file
 def load_books(filename):
   bookshelf = []
-  with open(filename) as file:
+  with open(os.path.join("C:\\","Local Repos","ca_sorts","a_sorted_tale",filename)) as file:
       shelf = csv.DictReader(file)
       for book in shelf:
           # add your code here
-          
-          bookshelf.append(book)
+          print(book.get('title'))
+          # author_lower = {'author_lower' : book.get('author').lower()}
+          # title_lower = {'title_lower' : book.get('title').lower()}
+          book_new = {'title' : book.get('title'), 'author' : book.get('author'), 'title_lower' : book.get('title').lower(), 'author_lower' : book.get('author').lower()}
+          # book = book.append(title_lower)
+          bookshelf.append(book_new)
+          # print(book_new)
   return bookshelf
+
+# print(ord("z"))
+# print(ord("1"))
+# print(ord("A")) 
